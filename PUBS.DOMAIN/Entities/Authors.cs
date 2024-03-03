770b430fb6db9cedc862805414031ec4494d209b
Ernesto Saviñón <ernestonicolas2546@gmail.com>
2024-03-02 22:56:46 -0400

    Creacion de proyectos iniciales (Domain e Infrastructure)

---
+++ PUBS.DOMAIN/Entities/Authors.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PUBS.DOMAIN.Entities
+{
+    public class Authors
+    {
+        public string Id { get; set; }
+
+    }
+}
